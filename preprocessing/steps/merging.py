e5da9b53e50d5995c9530b10af754302d8774fa8
matvey sokolov <98530161+j0pl0p@users.noreply.github.com>
2026-06-02 14:24:18 +0300

    catboost

--- preprocessing/steps/merging.py
+++ preprocessing/steps/merging.py
@@ -6,6 +6,6 @@ def merge_2020_and_2022(df2020: pl.DataFrame, df2022: pl.DataFrame) -> pl.DataFr
     assert df2020.shape[1] == len(set(df2020.columns) & set(df2022.columns))
     return pl.concat(
         [df2020.with_columns(pl.lit(2020).alias('Year')),
-         df2022[df2022.columns].with_columns(pl.lit(2022).alias('Year'))],
+         df2022[df2020.columns].with_columns(pl.lit(2022).alias('Year'))],
         how='align'
     )
